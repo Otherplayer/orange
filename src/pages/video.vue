7de6e8f52c334692fd8ce33ccb859664af090c1f
otherplayer <osmk@qq.com>
2018-01-17 14:28:24 +0800

    fix

--- src/pages/video.vue
+++ src/pages/video.vue
@@ -12,7 +12,7 @@
 
     <video id='18-video' controls class="video-player">
       <!--<source v-bind:src="params.videoUrl" />-->
-      <source src="./../assets/pre-ad.mp4" type='video/mp4' />
+      <source src="http://10.71.224.80:8081/static/video.mp4" type='video/mp4' />
     </video>
 
     <div class="video-title">{{params.title}}</div>
@@ -40,7 +40,7 @@
                 '18-video',
                 '',
                 {
-                    layout:'browser',
+                    layout:'default',
                     playerInitCallback: function() {
                         console.log('Player "18-video" initiation called');
                     },
